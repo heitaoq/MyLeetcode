695c0ff6982186c314878813247f9e9faa846856
zll <812877906@qq.com>
2020-02-09 17:49:39 +0800

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,9 @@
 public class Main {
 
     public static void main(String args[]){
-        LRUCacheMethod2 cacheMethod2=new LRUCacheMethod2(2);
-        cacheMethod2.put(2,1);
-        cacheMethod2.put(1,1);
-        cacheMethod2.put(2,3);
-        cacheMethod2.put(4,1);
-        cacheMethod2.get(1);
-        cacheMethod2.get(2);
+        int[] test={-1,0,1,2,-1,-4};
+        FourSum fourSum=new FourSum();
+       System.out.printf(""+fourSum.fourSum(test,-1));
     }
+
 }
--- src/main/java/ThreeSum.java
+++ src/main/java/ThreeSum.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class threeSum {
+public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
@@ -34,7 +34,6 @@ public class threeSum {
                     j++;
                     k--;
                 }
-
             }
             i++;
         }

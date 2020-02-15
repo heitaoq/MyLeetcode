abc904a66d8efbb8b2d3573510715f61cefde978
zll <812877906@qq.com>
2020-02-15 20:32:42 +0800

    add lengthOfLongestSubstringTwoDistinct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@
 public class Main {
 
     public static void main(String args[]){
-        int[] test={-1,0,1,2,-1,-4};
-        FourSum fourSum=new FourSum();
-       System.out.printf(""+fourSum.fourSum(test,-1));
+        String str="eceba";
+        LengthOfLongestSubstringTwoDistinct lengthOfLongestSubstringTwoDistinct=new LengthOfLongestSubstringTwoDistinct();
+       System.out.printf(""+lengthOfLongestSubstringTwoDistinct.lengthOfLongestSubstringTwoDistinct(str));
     }
 
 }

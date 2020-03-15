5edc89627580d872ff093faf1b3111647113ad8d
zll <812877906@qq.com>
2020-03-15 18:10:33 +0800

    add

---
+++ src/main/java/easy/ReversePrint.java
@@ -0,0 +1,25 @@
+package easy;
+
+import common.ListNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+/*面试题06. 从尾到头打印链表*/
+
+public class ReversePrint {
+    public int[] reversePrint(ListNode head) {
+        LinkedList<Integer> stack=new LinkedList<>();
+        ListNode tmp =head;
+        while( tmp !=null){
+            stack.addLast(tmp.getVal());
+            tmp=tmp.getNext();
+        }
+        int[] res= new int[stack.size()];
+        for(int i=0;i<res.length;i++){
+            res[i]=stack.removeLast();
+        }
+        return res;
+
+    }
+}

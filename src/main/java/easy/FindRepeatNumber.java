3fb6b1a91ffbf4d7a2428884fa3f108bbe2b895f
zll <812877906@qq.com>
2020-03-19 08:53:51 +0800

    add

---
+++ src/main/java/easy/FindRepeatNumber.java
@@ -0,0 +1,28 @@
+package easy;
+/*找出数组中重复的数字。
+
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
+数组中某些数字是重复的，但不知道有几个数字重复了，
+也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+输入：
+[2, 3, 1, 0, 2, 5, 3]
+输出：2 或 3
+*/
+import java.util.Arrays;
+
+public class FindRepeatNumber {
+    public int findRepeatNumber(int[] nums) {
+        int tmp;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i) { //使前面的有序
+                if (nums[i] == nums[nums[i]]) return nums[i];
+                else {
+                    tmp = nums[i];
+                    nums[i] = nums[nums[i]];
+                    nums[tmp] = tmp;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/easy/MajorityElement.java
@@ -0,0 +1,19 @@
+package easy;
+/*
+输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+输出: 2
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。*/
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int x=0;
+        int cnt=0;
+        for(int num:nums){
+            if(cnt==0) x=num;
+            cnt+=(x==num?1:-1);
+        }
+        return x;
+    }
+}

2b2c187b1115b12dcd9c3bb1316543e77fb7a92a
zll <812877906@qq.com>
2020-03-20 22:45:11 +0800

    add

---
+++ src/main/java/easy/GetIntersectionNode.java
@@ -0,0 +1,25 @@
+package easy;
+/*
+输入两个链表，找出它们的第一个公共节点。
+如下面的两个链表：
+在节点 c1 开始相交。
+利用a+b+相交=b+a+相交即可*/
+public class GetIntersectionNode {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode nodeA=headA;
+        ListNode nodeB=headB;
+        while(nodeA != nodeB){
+            nodeA=nodeA==null?headB:nodeA.next;
+            nodeB=nodeB==null?headA:nodeB.next;
+        }
+        return nodeA;
+    }
+}

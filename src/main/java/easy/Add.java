8720eb1366ff229897b219947370d0597428244f
zll <812877906@qq.com>
2020-03-23 11:46:18 +0800

    add

---
+++ src/main/java/easy/Add.java
@@ -0,0 +1,13 @@
+package easy;
+/*写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+* */
+public class Add {
+    public int add(int a, int b) {
+        while (a!=0){
+             int tmp=a^b;
+             a=(a&b)<<1;//进位
+             b=tmp;
+        }
+        return b;
+    }
+}
---
+++ src/main/java/easy/IsSymmetric.java
@@ -0,0 +1,27 @@
+package easy;
+/*请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+*/
+public class IsSymmetric {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return root==null?true:recur(root.left,root.right);
+    }
+    public boolean recur(TreeNode left,TreeNode right){
+        if(left==null&&right==null) return true;
+        if(left==null||right==null||left.val!=right.val) return false;
+        return recur(left.left,right.right)&&recur(left.right,right.left);
+    }
+}
